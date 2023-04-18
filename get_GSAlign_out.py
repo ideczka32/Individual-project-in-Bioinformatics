5a9c53293b689733148cbe40054e33fc0341fb1b
ideczka32 <80394174+ideczka32@users.noreply.github.com>
2023-04-18 13:58:08 +0200

    Update get_GSAlign_out.py

--- get_GSAlign_out.py
+++ get_GSAlign_out.py
@@ -1,8 +1,14 @@
+'''this code counts the number of mutations from GSAlign output and later saves it into meg file'''
 import sys
 
 input_file = sys.argv[1]
 prefix_1 = sys.argv[2]
 prefix_2 = sys.argv[3]
+analys_ID = sys.argv[4]
+
+out_file_name = analys_ID + '.txt'
+
+print("GET GSALIGN CODE NOW")
 
 with open(input_file, 'r') as f:
     data = f.read()
@@ -16,8 +22,9 @@ with open(input_file, 'r') as f:
 #add the info necessary to build a meg file in the future
 info = prefix_1 + ' ' + prefix_2 + ' ' + str(mutation_ctr)
 print('INFO: ', info)
-with open('meg_file.txt', 'a') as meg_file:
+with open(out_file_name, 'a') as meg_file:
     meg_file.write(info)
+    meg_file.write('\n')
 
 
 
